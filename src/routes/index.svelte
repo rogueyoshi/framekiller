41a45c4585266bd2952c94a89ad1c7625db0bd16
Al Murray <mail@rogueyoshi.com>
2021-04-15 03:33:24 -0400

    Renamed and basic UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,71 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	let files: FileList;
+	let text: String;
+	$: promise = files ? files[0].text().then((_text) => (text = _text)) : new Promise(() => {});
+	$: columns = text ? text.split('\r\n')[0].split(',') : [];
+	$: data = columns ? columns.reduce((acc, curr) => ((acc[curr] = ''), acc), {}) : [];
+
+	let name: String;
+	let calculations: String[] = [];
+	let minimum;
+	let maximum;
 </script>
 
+{@debug text}
+
 <svelte:head>
-	<title>Hello world!</title>
+	<title>framekiller</title>
 </svelte:head>
 
-<main>
-	<h1>Hello world!</h1>
+<h1>framekiller</h1>
+<h2>Find safe-jumps, meaties, and other frame-kills automatically given a set of frame-data.</h2>
 
-	<Counter />
+<p>upload a frame-data csv:</p>
+<small>or select a Google Drive document.</small>
 
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
+<input type="file" bind:files />
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
+{#await promise then resolved}
+	{#if resolved}
+		<p>select the name column:</p>
+		{#if columns}
+			{#each columns as column}
+				<code>
+					<label>
+						<input type="radio" bind:group={name} value={column} />
+						<span>{column}</span>
+					</label>
+				</code>
+			{/each}
+			{#if name}
+				<p>select the calculation column(s):</p>
+				{#each columns as column}
+					{#if column != name}
+						<code>
+							<label>
+								<input type="checkbox" bind:group={calculations} value={column} />
+								<span>{column}</span>
+							</label>
+						</code>
+					{/if}
+				{/each}
+				{#if calculations.length}
+					<p>set the frame-search range:</p>
+					<label>
+						<input type="number" min="1" bind:value={minimum} />
+						<span>minimum {minimum || 'x'} frames.</span>
+					</label>
+					<label>
+						<input type="number" min="1" bind:value={maximum} />
+						<span>maximum {maximum || 'x'} frames.</span>
+					</label>
+					{#if minimum && maximum}
+						<p>results...</p>
+					{/if}
+				{/if}
+			{/if}
+		{/if}
+	{/if}
+{:catch error}
+	{error}
+{/await}
